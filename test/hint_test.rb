97276d6e2cfc554edca87a8b21dca40714da7c47
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2009-12-08 16:03:31 -0200

    Adding an extra test to ensure hints are not created by default.

--- test/hint_test.rb
+++ test/hint_test.rb
@@ -6,7 +6,13 @@ class ErrorTest < ActionView::TestCase
     simple_form_for @user do |f|
       concat f.input :name, :hint => 'Use with care...'
     end
-    puts output_buffer
     assert_select 'form span.hint', 'Use with care...'
+  end
+
+  test 'input should not generate a hint by default' do
+    simple_form_for @user do |f|
+      concat f.input :name
+    end
+    assert_no_select 'form span.hint'
   end
 end
