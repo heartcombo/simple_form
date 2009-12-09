77815d3cd431213934cbe02b13bdcacc85ce7de7
José Valim <jose.valim@gmail.com>
2009-12-09 14:41:42 -0200

    Commit detect_collection_methods scratch.

--- lib/simple_form/input.rb
+++ lib/simple_form/input.rb
@@ -10,7 +10,7 @@ module SimpleForm
       :time     => Mapping.new(:time_select, false, true),
       :password => Mapping.new(:password_field, false, false),
       :hidden   => Mapping.new(:hidden_field, false, false),
-      :select   => Mapping.new(:select, true, true),
+      :select   => Mapping.new(:collection_select, true, true),
       :radio    => Mapping.new(:collection_radio, true, false),
       # Do we need integer and numeric?
       :integer  => Mapping.new(:text_field, false, false),
@@ -24,14 +24,19 @@ module SimpleForm
         html_options = @options[:html] || {}
         html_options[:class] = default_css_classes(html_options[:class])
         @options[:options] ||= {}
-        @options[:collection] ||= boolean_collection
 
         mapping = MAPPINGS[@input_type]
         raise "Invalid input type #{@input_type.inspect}" unless mapping
 
         args = [ @attribute ]
-        args << @options[:collection] if mapping.collection
-        args << @options[:options]    if mapping.options
+
+        if mapping.collection
+          collection = @options[:collection] || boolean_collection
+          detect_collection_methods(collection, @options)
+          args.push(collection, @options[:value_method], @options[:label_method])
+        end
+
+        args << @options[:options] if mapping.options
         args << html_options
 
         send(mapping.method, *args)
@@ -41,8 +46,26 @@ module SimpleForm
         [['Yes', true], ['No', false]]
       end
 
-      def collection_radio(attribute, collection, html_options)
-        collection.inject('') do |result, (text, value)|
+      def detect_collection_methods(collection, options)
+        sample = collection.first
+
+        if sample.is_a?(Array) # TODO Test me
+          options[:label_method] ||= :first
+          options[:value_method] ||= :last
+        elsif sample.is_a?(String) # TODO Test me
+          options[:label_method] ||= :to_s
+          options[:value_method] ||= :to_s
+        else # TODO Implement collection label methods or something similar
+          options[:label_method] ||= :to_s
+          options[:value_method] ||= :to_s
+        end
+      end
+
+      def collection_radio(attribute, collection, value_method, text_method, html_options={})
+        collection.inject('') do |result, item|
+            value = item.send value_method
+            text  = item.send text_method
+
             result << radio_button(attribute, value, html_options) <<
                       label("#{attribute}_#{value}", text, :class => default_css_classes)
         end
