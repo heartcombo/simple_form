a6496419b8c293851b0b7858b1a7699df2955ecb
José Valim <jose.valim@gmail.com>
2009-12-20 23:42:29 +0100

    Update TODO

--- generators/simple_form_install/simple_form_install_generator.rb
+++ generators/simple_form_install/simple_form_install_generator.rb
@@ -2,10 +2,11 @@ class SimpleFormInstallGenerator < Rails::Generator::Base
 
   def manifest
     record do |m|
+      m.directory 'config/initializers'
       m.template  'simple_form.rb', 'config/initializers/simple_form.rb'
 
       m.directory 'config/locales'
-      m.template  locale_file, 'config/locales/simple_form.yml'
+      m.template  locale_file, 'config/locales/simple_form.en.yml'
     end
   end
 
