c0b5f8f78bdc563bca9d906331da4d73688e3a8d
fedesoria <fedesoria@gmail.com>
2010-10-03 19:46:41 -0500

    Check for text area for rows/cols

--- lib/simple_form/inputs/mapping_input.rb
+++ lib/simple_form/inputs/mapping_input.rb
@@ -14,9 +14,25 @@ module SimpleForm
 
       def input_html_options
         input_options = super
-        input_options[:cols]        ||= cols if cols
-        input_options[:rows]        ||= rows if rows
+        input_options[:cols]        ||= cols if input_type == :text and has_cols?
+        input_options[:rows]        ||= rows if input_type == :text and has_rows?
         input_options
+      end
+      
+      def has_cols?
+        options[:cols] != false && cols.present?
+      end
+
+      def cols
+        @cols ||= options[:cols] || translate(:cols)
+      end
+      
+      def has_rows?
+        options[:rows] != false && rows.present?
+      end
+
+      def rows
+        @rows ||= options[:rows] || translate(:rows)
       end
       
     private
