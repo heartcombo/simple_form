c0e196e5758e298dbc1fa7d007ddc12243318c19
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2010-11-21 13:50:36 -0200

    Refactor string input a bit

--- lib/simple_form/inputs/string_input.rb
+++ lib/simple_form/inputs/string_input.rb
@@ -4,13 +4,13 @@ module SimpleForm
       def input
         input_html_options[:size]      ||= [limit, SimpleForm.default_input_size].compact.min
         input_html_options[:maxlength] ||= limit if limit
-        input_html_options[:type]      ||= input_type unless input_type == :string
+        input_html_options[:type]      ||= input_type unless string?
 
         @builder.text_field(attribute_name, input_html_options)
       end
 
       def input_html_classes
-        input_type == :string ? super : super.unshift("string")
+        string? ? super : super.unshift("string")
       end
 
     protected
@@ -21,6 +21,10 @@ module SimpleForm
 
       def has_placeholder?
         placeholder_present?
+      end
+
+      def string?
+        input_type == :string
       end
     end
   end
