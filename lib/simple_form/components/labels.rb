da0463b9d6fe9f1b1dd15eb792bdb8ad5ba9ec6b
Rodrigo Navarro <rnavarro1@gmail.com>
2011-02-08 07:42:18 +0800

    Adding label class configuration option

--- lib/simple_form/components/labels.rb
+++ lib/simple_form/components/labels.rb
@@ -36,7 +36,7 @@ module SimpleForm
       end
 
       def label_html_options
-        label_options = html_options_for(:label, [input_type, required_class])
+        label_options = html_options_for(:label, [input_type, required_class, SimpleForm.label_class])
         label_options[:for] = options[:input_html][:id] if options.key?(:input_html) && options[:input_html].key?(:id)
         label_options
       end
