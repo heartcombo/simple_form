f9269c8dc9e03999a298133b07e3eddb70e3f5ab
Alexander Zubkov <sasha@sekrett.ru>
2011-03-01 22:52:10 +0300

    Map input[type=password] to StringInput

--- lib/simple_form/inputs/mapping_input.rb
+++ lib/simple_form/inputs/mapping_input.rb
@@ -4,7 +4,6 @@ module SimpleForm
     class MappingInput < Base
       extend MapType
 
-      map_type :password, :to => :password_field
       map_type :text,     :to => :text_area
       map_type :file,     :to => :file_field
 
