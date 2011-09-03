9cf66e09c8239f6c0d8629ced71e79d1d2bccbea
Rafael Mendonça França <rafaelmfranca@gmail.com>
2011-09-03 16:28:01 -0300

    Do not need this comment

--- lib/simple_form/inputs/base.rb
+++ lib/simple_form/inputs/base.rb
@@ -43,7 +43,7 @@ module SimpleForm
         @options            = options
         @required           = calculate_required
         @input_html_options = html_options_for(:input, input_html_classes).tap do |o|
-          o[:required]  = true if has_required? # Don't make this conditional on HTML5 here, because we want the CSS class to be set
+          o[:required]  = true if has_required?
           o[:disabled]  = true if disabled?
           o[:autofocus] = true if has_autofocus? && SimpleForm.html5
         end
