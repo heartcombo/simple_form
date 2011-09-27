32b3227adba6d7934a441551733eb719afcfde01
Rafael Mendonça França <rafaelmfranca@gmail.com>
2011-09-27 00:56:01 -0300

    Generate configuration with browser_validations disabled.
    Closes #319

--- lib/generators/simple_form/templates/config/initializers/simple_form.rb
+++ lib/generators/simple_form/templates/config/initializers/simple_form.rb
@@ -49,7 +49,7 @@ SimpleForm.setup do |config|
 
   # Tell browsers whether to use default HTML5 validations (novalidate option).
   # Default is enabled.
-  # config.browser_validations = true
+  config.browser_validations = false
 
   # Determines whether HTML5 types (:email, :url, :search, :tel) and attributes
   # (e.g. required) are used or not. True by default.
