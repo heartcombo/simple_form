ab7cd70e427583322cb640f9f3107108c7a84dd0
Semyon Perepelitsa <sema@sema.in>
2011-12-06 16:16:31 +0800

    Group label is now detected

--- test/inputs_test.rb
+++ test/inputs_test.rb
@@ -980,7 +980,7 @@ class InputTest < ActionView::TestCase
   test 'input should have grouped options' do
     with_input_for @user, :name, :grouped_select,
                    :collection => [['Authors', ['Jose', 'Carlos']], ['General', ['Bob', 'John']]],
-                   :group_method => :last, :group_label_method => :first
+                   :group_method => :last
     assert_select 'select.grouped_select#user_name' do
       assert_select 'optgroup[label=Authors]' do
         assert_select 'option', 'Jose'
