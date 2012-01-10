d719e47c187108cbc9493317e3f7138bd035b249
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2012-01-10 19:00:14 -0200

    Use similar api for hints and errors, and cache hint to avoid double lookup

--- lib/simple_form/components/hints.rb
+++ lib/simple_form/components/hints.rb
@@ -3,11 +3,15 @@ module SimpleForm
     # Needs to be enabled in order to do automatic lookups.
     module Hints
       def hint
-        if options[:hint] == true
+        @hint ||= if options[:hint] == true
           translate(:hints)
         else
           options[:hint]
         end
+      end
+
+      def has_hint?
+        hint.present?
       end
     end
   end
