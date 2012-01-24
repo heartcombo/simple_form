02ffe40bcc9d11f644a6bd42401aa1c46b73d64e
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2012-01-24 11:48:38 -0200

    Add test to support grouped collection with association

--- test/inputs/grouped_collection_input_test.rb
+++ test/inputs/grouped_collection_input_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 
 class GroupedCollectionInputTest < ActionView::TestCase
   test 'grouped collection accepts array collection form' do
-    with_input_for @user, :name, :grouped_select,
+    with_input_for @user, :tag_ids, :grouped_select,
       :collection => [['Authors', ['Jose', 'Carlos']], ['General', ['Bob', 'John']]],
       :group_method => :last
 
-    assert_select 'select.grouped_select#user_name' do
+    assert_select 'select.grouped_select#user_tag_ids' do
       assert_select 'optgroup[label=Authors]' do
         assert_select 'option', 'Jose'
         assert_select 'option', 'Carlos'
@@ -21,11 +21,11 @@ class GroupedCollectionInputTest < ActionView::TestCase
   end
 
   test 'grouped collection accepts hash collection form' do
-    with_input_for @user, :name, :grouped_select,
+    with_input_for @user, :tag_ids, :grouped_select,
       :collection => { 'Authors' => ['Jose', 'Carlos'], 'General' => ['Bob', 'John'] },
       :group_method => :last
 
-    assert_select 'select.grouped_select#user_name' do
+    assert_select 'select.grouped_select#user_tag_ids' do
       assert_select 'optgroup[label=Authors]' do
         assert_select 'option', 'Jose'
         assert_select 'option', 'Carlos'
@@ -39,12 +39,12 @@ class GroupedCollectionInputTest < ActionView::TestCase
   end
 
   test 'grouped collection accepts group_label_method option' do
-    with_input_for @user, :name, :grouped_select,
+    with_input_for @user, :tag_ids, :grouped_select,
       :collection => { ['Jose', 'Carlos'] => 'Authors' },
       :group_method => :first,
       :group_label_method => :last
 
-    assert_select 'select.grouped_select#user_name' do
+    assert_select 'select.grouped_select#user_tag_ids' do
       assert_select 'optgroup[label=Authors]' do
         assert_select 'option', 'Jose'
         assert_select 'option', 'Carlos'
@@ -53,16 +53,38 @@ class GroupedCollectionInputTest < ActionView::TestCase
   end
 
   test 'grouped collection accepts label and value methods options' do
-    with_input_for @user, :name, :grouped_select,
+    with_input_for @user, :tag_ids, :grouped_select,
       :collection => { 'Authors' => ['Jose', 'Carlos'] },
       :group_method => :last,
       :label_method => :upcase,
       :value_method => :downcase
 
-    assert_select 'select.grouped_select#user_name' do
+    assert_select 'select.grouped_select#user_tag_ids' do
       assert_select 'optgroup[label=Authors]' do
         assert_select 'option[value=jose]', 'JOSE'
         assert_select 'option[value=carlos]', 'CARLOS'
+      end
+    end
+  end
+
+  test 'grouped collection with associations' do
+    tag_groups = [
+      TagGroup.new(1, "Group of Tags", [Tag.new(1, "Tag 1"), Tag.new(2, "Tag 2")]),
+      TagGroup.new(2, "Other group", [Tag.new(3, "Tag 3"), Tag.new(4,"Tag 4")])
+    ]
+
+    with_input_for @user, :tag_ids, :grouped_select,
+      :collection => tag_groups, :group_method => :tags
+
+    assert_select 'select.grouped_select#user_tag_ids' do
+      assert_select 'optgroup[label=Group of Tags]' do
+        assert_select 'option[value=1]', 'Tag 1'
+        assert_select 'option[value=2]', 'Tag 2'
+      end
+
+      assert_select 'optgroup[label=Other group]' do
+        assert_select 'option[value=3]', 'Tag 3'
+        assert_select 'option[value=4]', 'Tag 4'
       end
     end
   end
