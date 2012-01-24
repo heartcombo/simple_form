023bc25e919a7320683ee5d54e2e2ed43b49a167
José Valim <jose.valim@gmail.com>
2012-01-24 19:41:56 +0100

    Make explicit this wrapper is the default.

--- lib/generators/simple_form/templates/config/initializers/simple_form.rb
+++ lib/generators/simple_form/templates/config/initializers/simple_form.rb
@@ -5,7 +5,7 @@ SimpleForm.setup do |config|
   # wrapper, change the order or even add your own to the
   # stack. The options given below are used to wrap the
   # whole input.
-  config.wrappers :class => :input, :hint_class => :field_with_hint, :error_class => :field_with_errors do |b|
+  config.wrappers :default, :class => :input, :hint_class => :field_with_hint, :error_class => :field_with_errors do |b|
     ## Extensions enabled by default
     # Any of these extensions can be disabled for a
     # given input by passing: `f.input EXTENSION_NAME => false`.
