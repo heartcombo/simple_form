a0ea7e4e0cd1e20cd4cae2991ce4f03c7746ec14
Eric Hochberger <eric@mediavine.com>
2012-02-29 16:36:25 -0500

    Allow inline_label for booleans

--- lib/simple_form/inputs/boolean_input.rb
+++ lib/simple_form/inputs/boolean_input.rb
@@ -5,7 +5,7 @@ module SimpleForm
         if nested_boolean_style?
           build_hidden_field_for_checkbox +
             template.label_tag(nil, :class => "checkbox") {
-              build_check_box_without_hidden_field
+              build_check_box_without_hidden_field + options[:inline_label]
             }
         else
           build_check_box
