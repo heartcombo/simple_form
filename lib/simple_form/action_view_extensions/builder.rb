1ba09961356321cdb1b557a793f883841c26c15f
George Hemmings <gh@georgehemmings.com>
2012-03-09 00:49:27 +0000

    Fix checked radio button issue when value is false

--- lib/simple_form/action_view_extensions/builder.rb
+++ lib/simple_form/action_view_extensions/builder.rb
@@ -216,7 +216,8 @@ module SimpleForm
         html_options = html_options.dup
 
         [:checked, :selected, :disabled].each do |option|
-          next unless current_option = options[option]
+          current_option = options[option]
+          next if current_option.nil?
 
           accept = if current_option.respond_to?(:call)
             current_option.call(item)
