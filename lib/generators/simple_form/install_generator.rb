651559d694e2db98e3c394fa529008b9adea412c
Nathan W. McSween <nwmcsween@gmail.com>
2012-06-26 23:07:42 -0700

    Constantize frameworks and engines, add zurb foundation

--- lib/generators/simple_form/install_generator.rb
+++ lib/generators/simple_form/install_generator.rb
@@ -1,36 +1,63 @@
 module SimpleForm
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      attr_accessor :engine, :framework
+
+      ENGINES     = %w(erb haml slim)
+      FRAMEWORKS  = %w(bootstrap foundation)
+
       desc "Copy SimpleForm default files"
       source_root File.expand_path('../templates', __FILE__)
-      class_option :template_engine, :desc => 'Template engine to be invoked (erb, haml or slim).'
-      class_option :bootstrap, :type => :boolean, :desc => 'Add the Twitter Bootstrap wrappers to the SimpleForm initializer.'
+      class_option  :template_engine, :aliases => '-e',
+                    :desc => "Template engine to be invoked, supported: #{FRAMEWORKS.join(', ')}.",
+                    :type => :string
+      class_option  :framework, :aliases => '-b',
+                    :desc => "Framework to be invoked, supported: #{ENGINES.join(', ')}.",
+                    :type => :string
 
-      def info_bootstrap
-        return if options.bootstrap?
-        puts "SimpleForm 2 supports Twitter bootstrap. In case you want to " \
-          "generate bootstrap configuration, please re-run this " \
-          "generator passing --bootstrap as option."
+      def iniitialize
+        self.engine    = options[:template_engine]
+        self.framework = options[:framework]
+      end
+
+      def info
+        unless framework
+          say 'SimpleForm supports:'
+          say "#{FRAMEWORKS.join(', ')}"
+          say 'to install a framework specifiy'
+          say '(--framework || -b) framework'
+
+          unless FRAMEWORKS.include?(framework)
+            say 'SimpleForm does not support'
+            say "the framework named: #{framework}"
+          end
+        end
+
+        unless ENGINES.include?(engine)
+          say 'SimpleForm does not support'
+          say "the template engine named: #{engine}"
+        end
       end
 
       def copy_config
         template "config/initializers/simple_form.rb"
 
-        if options[:bootstrap]
-          template "config/initializers/simple_form_bootstrap.rb"
+        if FRAMEWORKS.include?(framework)
+          template "config/initializers/simple_form_#{framework}.rb"
         end
 
         directory 'config/locales'
       end
 
       def copy_scaffold_template
-        engine = options[:template_engine]
-        copy_file "_form.html.#{engine}", "lib/templates/#{engine}/scaffold/_form.html.#{engine}"
+        if ENGINES.include?(engine)
+          copy_file "_form.html.#{engine}", "lib/templates/#{engine}/scaffold/_form.html.#{engine}"
+        end
       end
 
       def show_readme
-        if behavior == :invoke && options.bootstrap?
-          readme "README"
+        if behavior == :invoke && FRAMEWORKS.include?(framework)
+          readme "#{framework}.txt"
         end
       end
     end
