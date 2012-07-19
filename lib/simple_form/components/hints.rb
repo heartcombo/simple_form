edf917d742a0483c33024a39806134ffd10db18d
Sam Woodard <sam.h.woodard@gmail.com>
2012-07-19 16:22:17 -0700

    fix in case translate returns an object

--- lib/simple_form/components/hints.rb
+++ lib/simple_form/components/hints.rb
@@ -6,7 +6,7 @@ module SimpleForm
         @hint ||= begin
           hint = options[:hint]
           hint_content = hint.is_a?(String) ? hint : translate(:hints)
-          hint_content.html_safe if hint_content
+          hint_content.html_safe if hint_content && hint_content.is_a?(String)
         end
       end
 
