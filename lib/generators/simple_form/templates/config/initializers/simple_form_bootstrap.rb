daba58c49cc84cfe99b30472e108d0ef4e419855
pedrozath <pedrozath@gmail.com>
2013-08-21 08:14:50 -0300

    updated simple_form_bootstrap.rb for bootstrap-3.0.0

--- lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap.rb
+++ lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap.rb
@@ -1,6 +1,6 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.wrappers :bootstrap, tag: 'div', class: 'control-group', error_class: 'error' do |b|
+  config.wrappers :bootstrap, tag: 'div', class: 'form-group', error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label
@@ -11,7 +11,7 @@ SimpleForm.setup do |config|
     end
   end
 
-  config.wrappers :prepend, tag: 'div', class: "control-group", error_class: 'error' do |b|
+  config.wrappers :prepend, tag: 'div', class: "form-group", error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label
@@ -24,7 +24,7 @@ SimpleForm.setup do |config|
     end
   end
 
-  config.wrappers :append, tag: 'div', class: "control-group", error_class: 'error' do |b|
+  config.wrappers :append, tag: 'div', class: "form-group", error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label
