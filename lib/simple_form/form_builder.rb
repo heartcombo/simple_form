5800eec2c4916d1fdb7071bbac7d80160c2f4fbb
Alexander <aheaven87@gmail.com>
2013-09-30 21:38:34 +0300

    #897: support rails 4 association conditions

--- lib/simple_form/form_builder.rb
+++ lib/simple_form/form_builder.rb
@@ -184,9 +184,7 @@ module SimpleForm
 
       options[:as] ||= :select
       options[:collection] ||= options.fetch(:collection) {
-        conditions = reflection.options[:conditions]
-        conditions = conditions.call if conditions.respond_to?(:call)
-        reflection.klass.where(conditions).order(reflection.options[:order])
+        reflection.scope ? reflection.klass.class_eval(&reflection.scope) : reflection.klass.all
       }
 
       attribute = case reflection.macro
