3d5359511291d493f7aa4852b815b96b4f66066f
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-03-14 10:57:46 -0300

    Store the component on the instance
    We already own the component so we don't need to ask for it again

--- lib/simple_form/wrappers/single.rb
+++ lib/simple_form/wrappers/single.rb
@@ -3,22 +3,20 @@ module SimpleForm
     # `Single` is an optimization for a wrapper that has only one component.
     class Single < Many
       def initialize(name, wrapper_options = {}, options = {})
-        super(name, [Leaf.new(name, options)], wrapper_options)
+        @component = Leaf.new(name, options)
+
+        super(name, [@component], wrapper_options)
       end
 
       def render(input)
         options = input.options
         if options[namespace] != false
-          content = component.render(input)
+          content = @component.render(input)
           wrap(input, options, content) if content
         end
       end
 
       private
-
-      def component
-        components.first
-      end
 
       def html_options(options)
         [:label, :input].include?(namespace) ? {} : super
