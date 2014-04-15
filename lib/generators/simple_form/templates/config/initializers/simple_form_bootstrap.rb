39299fbe153acd4022522706502a93a6f312d88c
Robbert Klarenbeek <robbertkl@renbeek.nl>
2014-04-15 22:36:31 +0200

    Add proper error_notification_class for Bootstrap 3

--- lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap.rb
+++ lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap.rb
@@ -1,5 +1,6 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
+  config.error_notification_class = 'alert alert-danger'
   config.button_class = 'btn btn-default'
   config.boolean_label_class = nil
 
