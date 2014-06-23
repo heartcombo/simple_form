276e06e1d1c8f3e2e839ed09d3bc97f854323f99
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-06-23 15:59:56 -0300

    Any truthy value is valid to enable an optional component

--- lib/generators/simple_form/templates/config/initializers/simple_form.rb
+++ lib/generators/simple_form/templates/config/initializers/simple_form.rb
@@ -22,9 +22,9 @@ SimpleForm.setup do |config|
     b.use :placeholder
 
     ## Optional extensions
-    # They are disabled unless you pass `f.input EXTENSION_NAME => :lookup`
+    # They are disabled unless you pass `f.input EXTENSION_NAME => true`
     # to the input. If so, they will retrieve the values from the model
-    # if any exists. If you want to enable the lookup for any of those
+    # if any exists. If you want to enable any of those
     # extensions by default, you can change `b.optional` to `b.use`.
 
     # Calculates maxlength from length validations for string inputs
