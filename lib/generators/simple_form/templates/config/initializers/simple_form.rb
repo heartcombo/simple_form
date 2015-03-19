58e404767a3982ecfecd56e1a6d3a2806d7b91f9
Uros Jurglic <jurglic@gmail.com>
2015-03-19 10:16:55 +0100

    Update docs regarding item_wrapper_tag
    Item_wrapper_tag configuration option in 3.x behaves independently,
    without any limitations imposed by nested boolean_style as mentioned in
    the documentation.
    
    Update readme and a comment inside the initializer.
    
    Closes #1224.

--- lib/generators/simple_form/templates/config/initializers/simple_form.rb
+++ lib/generators/simple_form/templates/config/initializers/simple_form.rb
@@ -90,8 +90,7 @@ SimpleForm.setup do |config|
   # config.collection_wrapper_class = nil
 
   # You can wrap each item in a collection of radio/check boxes with a tag,
-  # defaulting to :span. Please note that when using :boolean_style = :nested,
-  # SimpleForm will force this option to be a label.
+  # defaulting to :span.
   # config.item_wrapper_tag = :span
 
   # You can define a class to use in all item wrappers. Defaulting to none.
