7da6ea74a047612954fdf54ec9a37407f85edc4b
Ardian Yuli Setyanto <ardianys@gmail.com>
2015-04-09 09:35:59 +0700

    minor update to fix alignment
    in bootstrap every field input is inside div with grid definition in it's class.

--- lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap.rb
+++ lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap.rb
@@ -126,7 +126,7 @@ SimpleForm.setup do |config|
     b.use :html5
     b.optional :readonly
     b.use :label, class: 'control-label'
-    b.wrapper tag: 'div', class: 'form-inline' do |ba|
+    b.wrapper tag: 'div', class: 'form-inline col-sm-10' do |ba|
       ba.use :input, class: 'form-control'
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
