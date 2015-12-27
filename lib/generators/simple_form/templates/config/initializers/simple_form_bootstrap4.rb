eb409484e2db2508ed91870074e00ee16c0ec31d
Tobias Jordans <t@tobiasjordans.de>
2015-12-27 15:27:50 +0100

    Update defaults for bootstrap 4: Button
    http://v4-alpha.getbootstrap.com/migration/#forms says:
    > Renamed .btn-default to .btn-secondary.
    
    So the default button is .btn now with now additional class.

--- lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap4.rb
+++ lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap4.rb
@@ -1,7 +1,7 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
   config.error_notification_class = 'alert alert-danger'
-  config.button_class = 'btn btn-default'
+  config.button_class = 'btn'
   config.boolean_label_class = nil
 
   config.wrappers :vertical_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
