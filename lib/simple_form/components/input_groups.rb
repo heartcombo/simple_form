cecff34cd906956f28c817964957a1f1103e73ad
m5o <m5o@gmx.com>
2018-03-07 14:05:45 +0100

    improve readability by extract inner span tag’s

--- lib/simple_form/components/input_groups.rb
+++ lib/simple_form/components/input_groups.rb
@@ -4,13 +4,15 @@ module SimpleForm
     module InputGroups
       module Prepends
         def prepend(wrapper_options = nil)
-          template.content_tag(:div, content_tag(:span, options[:prepend], class: "input-group-text"), class: "input-group-prepend")
+          span_tag = content_tag(:span, options[:prepend], class: "input-group-text")
+          template.content_tag(:div, span_tag, class: "input-group-prepend")
         end
       end
 
       module Appends
         def append(wrapper_options = nil)
-          template.content_tag(:div, content_tag(:span, options[:append], class: "input-group-text"), class: "input-group-append")
+          span_tag = content_tag(:span, options[:append], class: "input-group-text")
+          template.content_tag(:div, span_tag, class: "input-group-append")
         end
       end
     end
