f38c6a1d3acca8e3b5bd650fd92a6466d468eb71
Vasiliy Ermolovich <younash@gmail.com>
2022-07-06 14:01:46 +0300

    Split country and timezone inputs to fix deprecated way of passing priority countries to country input.
    It still uses SimpleForm's PriorityInput for backward compatibility.

--- test/inputs/time_zone_input_test.rb
+++ test/inputs/time_zone_input_test.rb
@@ -2,21 +2,7 @@
 # encoding: UTF-8
 require 'test_helper'
 
-class PriorityInputTest < ActionView::TestCase
-  test 'input generates a country select field' do
-    with_input_for @user, :country, :country
-    assert_select 'select#user_country'
-    assert_select 'select option[value=BR]', 'Brazil'
-    assert_no_select 'select option[value=""][disabled=disabled]'
-  end
-
-  test 'input generates a country select with SimpleForm default' do
-    swap SimpleForm, country_priority: [ 'Brazil' ] do
-      with_input_for @user, :country, :country
-      assert_select 'select option[value="---------------"][disabled=disabled]'
-    end
-  end
-
+class TimeZoneInputTest < ActionView::TestCase
   test 'input generates a time zone select field' do
     with_input_for @user, :time_zone, :time_zone
     assert_select 'select#user_time_zone'
@@ -36,14 +22,14 @@ class PriorityInputTest < ActionView::TestCase
     assert_no_select 'select option[value=""]', /^$/
   end
 
-  test 'priority input does generate select element with required html attribute' do
-    with_input_for @user, :country, :country
+  test 'input does generate select element with required html attribute' do
+    with_input_for @user, :time_zone, :time_zone
     assert_select 'select.required'
     assert_select 'select[required]'
   end
 
-  test 'priority input does generate select element with aria-required html attribute' do
-    with_input_for @user, :country, :country
+  test 'input does generate select element with aria-required html attribute' do
+    with_input_for @user, :time_zone, :time_zone
     assert_select 'select.required'
     assert_select 'select[aria-required]'
   end
