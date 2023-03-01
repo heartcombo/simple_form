1d9890e82af1b3d32c030f9ebb6b181eafe7f859
Tom Rothe <motine@users.noreply.github.com>
2023-03-01 09:02:07 +0100

    pass base object to human_attribute_name in labels

--- lib/simple_form/components/labels.rb
+++ lib/simple_form/components/labels.rb
@@ -74,7 +74,7 @@ module SimpleForm
         if SimpleForm.translate_labels && (translated_label = translate_from_namespace(:labels))
           translated_label
         elsif object.class.respond_to?(:human_attribute_name)
-          object.class.human_attribute_name(reflection_or_attribute_name.to_s)
+          object.class.human_attribute_name(reflection_or_attribute_name.to_s, { base: object })
         else
           attribute_name.to_s.humanize
         end
