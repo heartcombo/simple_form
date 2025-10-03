6f866e0b0a5b7adf2b1aaa097a41f8b1c7632a83
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2025-10-03 13:01:06 -0300

    Fix country_select tests looking for separator option
    They changed to use an `<hr>` element now rather than a disabled option
    with `-` as separator.
    
    https://github.com/countries/country_select/pull/229

--- test/inputs/country_input_test.rb
+++ test/inputs/country_input_test.rb
@@ -3,23 +3,31 @@
 require 'test_helper'
 
 class CountryInputTest < ActionView::TestCase
+  COUNTRY_SELECT_SEPARATOR =
+    if Gem::Version.new(CountrySelect::VERSION) >= Gem::Version.new("11.0.0")
+      "hr"
+    else
+      'option[value="---------------"][disabled=disabled]'
+    end
+
   test 'input generates a country select field' do
     with_input_for @user, :country, :country
     assert_select 'select#user_country'
     assert_select 'select option[value=BR]', 'Brazil'
     assert_no_select 'select option[value=""][disabled=disabled]'
+    assert_no_select "select #{COUNTRY_SELECT_SEPARATOR}"
   end
 
   test 'input generates a country select with SimpleForm default' do
     swap SimpleForm, country_priority: [ 'Brazil' ] do
       with_input_for @user, :country, :country
-      assert_select 'select option[value="BR"] + option[value="---------------"][disabled=disabled]'
+      assert_select %(select option[value="BR"] + #{COUNTRY_SELECT_SEPARATOR})
     end
   end
 
   test 'input generates a country select using options priority' do
     with_input_for @user, :country, :country, priority: [ 'Ukraine' ]
-    assert_select 'select option[value="UA"] + option[value="---------------"][disabled=disabled]'
+    assert_select %(select option[value="UA"] + #{COUNTRY_SELECT_SEPARATOR})
   end
 
   test 'input does generate select element with required html attribute' do
