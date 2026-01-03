bdc817e4d9e6c3bb14695efb79e96ad15dc2ccc7
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2026-01-03 10:58:56 -0300

    Reuse resolve value implementation across other validator checks
    The shared logic can be reused to handle numericality and format
    validators as well, instead of custom handling for each, so it better
    matches Rails own implementation.

--- lib/simple_form/components/min_max.rb
+++ lib/simple_form/components/min_max.rb
@@ -19,32 +19,22 @@ module SimpleForm
 
       def minimum_value(validator_options)
         if integer? && validator_options.key?(:greater_than)
-          evaluate_numericality_validator_option(validator_options[:greater_than]) + 1
+          resolve_validator_value(validator_options[:greater_than]) + 1
         else
-          evaluate_numericality_validator_option(validator_options[:greater_than_or_equal_to])
+          resolve_validator_value(validator_options[:greater_than_or_equal_to])
         end
       end
 
       def maximum_value(validator_options)
         if integer? && validator_options.key?(:less_than)
-          evaluate_numericality_validator_option(validator_options[:less_than]) - 1
+          resolve_validator_value(validator_options[:less_than]) - 1
         else
-          evaluate_numericality_validator_option(validator_options[:less_than_or_equal_to])
+          resolve_validator_value(validator_options[:less_than_or_equal_to])
         end
       end
 
       def find_numericality_validator
         find_validator(:numericality)
-      end
-
-      def evaluate_numericality_validator_option(option)
-        if option.is_a?(Numeric)
-          option
-        elsif option.is_a?(Symbol)
-          object.send(option)
-        elsif option.respond_to?(:call)
-          option.call(object)
-        end
       end
     end
   end
--- lib/simple_form/components/pattern.rb
+++ lib/simple_form/components/pattern.rb
@@ -15,20 +15,12 @@ module SimpleForm
         if pattern.is_a?(String)
           pattern
         elsif (pattern_validator = find_pattern_validator) && (with = pattern_validator.options[:with])
-          evaluate_format_validator_option(with).source
+          resolve_validator_value(with).source
         end
       end
 
       def find_pattern_validator
         find_validator(:format)
-      end
-
-      def evaluate_format_validator_option(option)
-        if option.respond_to?(:call)
-          option.call(object)
-        else
-          option
-        end
       end
     end
   end
